f5e2732fe4ecf12be81a35150431b3d9bdaf50ce
shibata <pun_pun_3@icloud.com>
2023-10-24 00:48:25 +0900

    Chapter3の追加

--- package/src/components/introduction/index.tsx
+++ package/src/components/introduction/index.tsx
@@ -15,6 +15,7 @@ import {
 } from "../../const/CommonStyledComponent"
 import { Chapter1 } from "../Chapters/Chapter1"
 import { Chapter2 } from "../Chapters/Chapter2"
+import { Chapter3 } from "../Chapters/Chapter3"
 
 export const Introduction: FC = () => (
   <Root>
@@ -107,6 +108,7 @@ export const Introduction: FC = () => (
       <div>
         <Chapter1></Chapter1>
         <Chapter2></Chapter2>
+        <Chapter3></Chapter3>
       </div>
     </div>
   </Root>
